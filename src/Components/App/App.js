1a7fbe7f84c90e5d2b129200fa25f43a48aad1cd
Ochomozone <112177400+Ochomozone@users.noreply.github.com>
2023-04-07 23:06:15 +0300

    step 73 complete

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack (track) {
@@ -59,12 +60,16 @@ savePlaylist() {
   
 }
 
+search (searchTerm) {
+  console.log(searchTerm);
+}
+
   render () {
     return (
       <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-    <SearchBar />
+    <SearchBar onSearch={this.search}/>
     <div className="App-playlist">
       <SearchResults searchResults={this.state.searchResults} 
       onAdd={this.addTrack} />
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor (props) {
+        super(props);
+        
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    search () {
+        this.props.onSearch(this.state.searchTerm );
+    }
+
+    handleTermChange (e) {
+        this.setState({searchTerm: e.target.value});
+    }
+
     render () {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
+                <input placeholder="Enter A Song, Album, or Artist"
+                onChange={this.handleTermChange} />
                 <button className="SearchButton">SEARCH</button>
             </div>
         )
